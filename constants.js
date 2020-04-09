fcea2ba6a8c819c8d1c2ee42c962f47657f2c517
Sean Sullivan <dev@seanmedia.ca>
2020-04-09 21:53:21 +0000

    Complete function to retrieve public IP address

---
+++ constants.js
@@ -0,0 +1,5 @@
+// constants.js
+
+const IP_URL = "https://api.ipify.org?format=json";
+
+module.exports = { IP_URL };
--- iss.js
+++ iss.js
@@ -1,1 +1,27 @@
 // iss.js
+
+const request = require('request');
+const { IP_URL } = require('./constants');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) { 
+  // use request to fetch IP address from JSON API
+  request(IP_URL, (err, resp, body) => {
+    if (err) {
+      callback(err, null);
+    } if (resp.statusCode == 200 && body) {
+      const obj = JSON.parse(body);
+      const ipAddress = obj["ip"];
+      callback(null, ipAddress);
+    }
+  })
+}
+
+module.exports = { fetchMyIP };
